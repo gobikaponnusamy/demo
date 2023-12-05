f64e5c4b847217fc019b813b910e81cd134f08b8
Gobika Ponnusamy <113342365+gobikaponnusamy@users.noreply.github.com>
2023-12-05 14:31:44 +0530

    Update update_readme.sh
    fgfgfgf

--- update_readme.sh
+++ update_readme.sh
@@ -10,18 +10,14 @@ pr_description=$(git log -1 --pretty=%b)
 # Create the output directory if it doesn't exist
 mkdir -p output
 
-# Check for specific cases and display the title accordingly
-if [ "$pr_title" == "files added" ]; then
-  echo "PR Title: $pr_title" >> output/readme.txt
-else
-  # Check if the description is not empty before appending to the readme.txt file
-  if [ -n "$pr_description" ]; then
-    echo "PR Title: $pr_title" >> output/readme.txt
-    echo "PR Description: $pr_description" >> output/readme.txt
-    echo "=========================" >> output/readme.txt
-  else
-    echo "PR Title: $pr_title" >> output/readme.txt
-    echo "=========================" >> output/readme.txt
-  fi
-fi
+# Display the original title without modification
+echo "### Pull Request Title: $pr_title" >> output/readme.txt
 
+# Check if the description is not empty before appending to the readme.txt file
+if [ -n "$pr_description" ]; then
+  echo "**Description:**" >> output/readme.txt
+  echo "$pr_description" >> output/readme.txt
+  echo "=========================" >> output/readme.txt
+else
+  echo "=========================" >> output/readme.txt
+fi

cea0d236d1de786a722292678abfc41a38c7f08e
Gobika Ponnusamy <113342365+gobikaponnusamy@users.noreply.github.com>
2023-12-05 14:44:47 +0530

    Update update_readme.sh

--- update_readme.sh
+++ update_readme.sh
@@ -4,21 +4,19 @@
 cd "$(git rev-parse --show-toplevel)"
 
 # Get the pull request title and description
-pr_title=$(git log -1 --pretty=%s)
-pr_description=$(git log -1 --pretty=%b)
+pr_title=$(git log --format=%s -n 1)
+pr_description=$(git log --format=%b -n 1)
 
 # Create the output directory if it doesn't exist
 mkdir -p output
 
-# Display the original title without modification
-echo "### Pull Request Title: $pr_title" >> output/readme.txt
-
 # Check if the description is not empty before appending to the readme.txt file
 if [ -n "$pr_description" ]; then
+  echo "### Pull Request Title: $pr_title" >> output/readme.txt
   echo "**Description:**" >> output/readme.txt
   echo "$pr_description" >> output/readme.txt
   echo "=========================" >> output/readme.txt
 else
+  echo "### Pull Request Title: $pr_title" >> output/readme.txt
   echo "=========================" >> output/readme.txt
 fi
-

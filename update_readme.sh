44f2d0cd019dbdc4beb181ff7a943341a19a3d45
Gobika Ponnusamy <113342365+gobikaponnusamy@users.noreply.github.com>
2023-12-05 14:22:37 +0530

    Update update_readme.sh
    dfw

--- update_readme.sh
+++ update_readme.sh
@@ -10,7 +10,12 @@ pr_description=$(git log -1 --pretty=%b)
 # Create the output directory if it doesn't exist
 mkdir -p output
 
-# Append the pull request title and description to the readme.txt file
-echo "PR Title: $pr_title" >> output/readme.txt
-echo "PR Description: $pr_description" >> output/readme.txt
-echo "=========================" >> output/readme.txt
+# Check if the description is empty before appending to the readme.txt file
+if [ -n "$pr_description" ]; then
+  echo "PR Title: $pr_title" >> output/readme.txt
+  echo "PR Description: $pr_description" >> output/readme.txt
+  echo "=========================" >> output/readme.txt
+else
+  echo "PR Title: $pr_title" >> output/readme.txt
+  echo "=========================" >> output/readme.txt
+fi

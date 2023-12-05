16cb5d405b69ca4b4732ec4b8796328eb60d3265
Gobika Ponnusamy <113342365+gobikaponnusamy@users.noreply.github.com>
2023-12-05 14:54:21 +0530

    Update update_readme.sh

--- update_readme.sh
+++ update_readme.sh
@@ -7,20 +7,16 @@ cd "$(git rev-parse --show-toplevel)"
 pr_title=$(git log --format=%s -n 1)
 pr_description=$(git log --format=%b -n 1)
 
-# Extract the readable title from the commit message
-readable_title=$(echo "$pr_title" | sed -n 's/Merge \(.*\) into .*/\1/p')
-
 # Create the output directory if it doesn't exist
 mkdir -p output
 
 # Check if the description is not empty before appending to the readme.txt file
 if [ -n "$pr_description" ]; then
-  echo "### Pull Request Title: $readable_title" >> output/readme.txt
+  echo "### Pull Request Title: $pr_title" >> output/readme.txt
   echo "**Description:**" >> output/readme.txt
   echo "$pr_description" >> output/readme.txt
   echo "=========================" >> output/readme.txt
 else
-  echo "### Pull Request Title: $readable_title" >> output/readme.txt
+  echo "### Pull Request Title: $pr_title" >> output/readme.txt
   echo "=========================" >> output/readme.txt
 fi
-

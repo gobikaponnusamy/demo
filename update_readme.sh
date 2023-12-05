430c2bad311f2fad3eca9909d62c7330bc53f8a4
Gobika Ponnusamy <113342365+gobikaponnusamy@users.noreply.github.com>
2023-12-05 14:25:24 +0530

    Update update_readme.sh
    fd

--- update_readme.sh
+++ update_readme.sh
@@ -10,12 +10,17 @@ pr_description=$(git log -1 --pretty=%b)
 # Create the output directory if it doesn't exist
 mkdir -p output
 
-# Check if the description is empty before appending to the readme.txt file
-if [ -n "$pr_description" ]; then
+# Check for specific cases and display the title accordingly
+if [ "$pr_title" == "files added" ]; then
   echo "PR Title: $pr_title" >> output/readme.txt
-  echo "PR Description: $pr_description" >> output/readme.txt
-  echo "=========================" >> output/readme.txt
 else
-  echo "PR Title: $pr_title" >> output/readme.txt
-  echo "=========================" >> output/readme.txt
+  # Check if the description is not empty before appending to the readme.txt file
+  if [ -n "$pr_description" ]; then
+    echo "PR Title: $pr_title" >> output/readme.txt
+    echo "PR Description: $pr_description" >> output/readme.txt
+    echo "=========================" >> output/readme.txt
+  else
+    echo "PR Title: $pr_title" >> output/readme.txt
+    echo "=========================" >> output/readme.txt
+  fi
 fi
